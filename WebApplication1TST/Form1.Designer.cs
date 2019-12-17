86fa06e4b32e46061c74b5e3409818180934bf6f
Viktor.Novosilniy@lanteria.net <Viktor.Novosilniy@lanteria.net>
2019-12-17 03:34:39 -0800

    2

--- WebApplication1TST/Form1.Designer.cs
+++ WebApplication1TST/Form1.Designer.cs
@@ -378,6 +378,8 @@ namespace WebApplication1TST
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Sharepoint Data Import Tool";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
